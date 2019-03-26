011c25a48fbe0e27f781ae067c718cb5416b931c
LeeSolMoe <warwick8300@gmail.com>
2019-03-26 18:27:49 +0900

    2019-03-26

--- java-project2-server/src/main/java/com/eomcs/lms/handler/HelloCommand.java
+++ java-project2-server/src/main/java/com/eomcs/lms/handler/HelloCommand.java
@@ -1,14 +1,44 @@
 package com.eomcs.lms.handler;
-
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import org.springframework.stereotype.Component;
 import com.eomcs.lms.context.RequestMapping;
+import com.eomcs.lms.domain.Board;
+import com.eomcs.lms.service.BoardService;
 
 @Component
 public class HelloCommand {
-
-  @RequestMapping("/hello")
-  public void execute(ServletResponse response) {
-    response.println("안녕하세요!");
+  
+  BoardService boardService;
+  
+  public HelloCommand(BoardService boardService) {
+    this.boardService = boardService;
   }
 
+  @RequestMapping("/hello")
+  public void list(ServletRequest request, ServletResponse response) throws IOException {
+    List<Board> boards = boardService.list();
+   
+    
+    PrintWriter out = response.getWriter();
+    out.println("<html><head><title>hello</title></head>");
+    out.println("<body><h1>Hello, world</h1>");
+    out.println("</body></html>");
+  }
+  
+
+
 }
+
+
+
+
+
+
+
+
+
+
